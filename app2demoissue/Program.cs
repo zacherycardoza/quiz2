839731e91810c13bac71f2b08207136989e3b0a0
yhlica <yli202@my.centennialcollege.ca>
2024-01-22 13:29:47 -0500

    Add project files.

---
+++ app2demoissue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace app2demoissue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
