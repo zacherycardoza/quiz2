734f642102d080acff3ebda61739c903bd3b63c2
mohannadabdoprof <159362592+mohannadabdoprof@users.noreply.github.com>
2024-02-09 15:46:48 -0500

    Revert "Add C# code to update TodoItems (#1)" (#5)
    This reverts commit b35ec86c952c348ba8063158ad4e86c0d75f214b.

--- app2demoissue/Program.cs
+++ app2demoissue/Program.cs
@@ -4,37 +4,7 @@
     {
         static void Main(string[] args)
         {
-            List<TodoItem> todoList = new List<TodoItem>
-        {
-            new TodoItem { Id = 1, Task = "Complete quiz#1", Completed = false },
-            new TodoItem { Id = 2, Task = "Study for exam", Completed = false }
-        };
-
-           
-            Console.WriteLine("Current TodoItems:");
-            DisplayTodoItems(todoList);
-
-           
-            AddTodoItem(todoList, new TodoItem { Id = 3, Task = "Complete quiz#2", Completed = false });
-
-         
-            Console.WriteLine("\nUpdated TodoItems:");
-            DisplayTodoItems(todoList);
-        }
-
-        static void AddTodoItem(List<TodoItem> todoList, TodoItem newItem)
-        {
-           
-            todoList.Add(newItem);
-        }
-
-        static void DisplayTodoItems(List<TodoItem> todoList)
-        {
-            foreach (var item in todoList)
-            {
-                Console.WriteLine($"[{item.Id}] {item.Task} - Completed: {item.Completed}");
-            }
+            Console.WriteLine("Hello, World!");
         }
     }
-
 }
--- app2demoissue/TodoItem.cs
+++
@@ -1,6 +0,0 @@
-class TodoItem
-{
-    public int Id { get; set; }
-    public string Task { get; set; }
-    public bool Completed { get; set; }
-}
